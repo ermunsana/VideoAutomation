8d84d63f2f5f848aaf02c08e214be6ee41a6c264
ermuun <125609935+ermunsana@users.noreply.github.com>
2026-01-02 16:37:12 +0100

    Update run_main.py

--- run_main.py
+++ run_main.py
@@ -3,12 +3,12 @@ import subprocess
 import sys
 import os
 
-# Function to run main.py
+
 def run_main_py():
     print("Running main.py to generate the video...")
     try:
-        # Ensure the virtual environment is activated
-        venv_python = "D:/Github/Tiktoker/venv/Scripts/python.exe"  # Adjust to match your Python executable
+
+        venv_python = "D:/Github/Tiktoker/venv/Scripts/python.exe"  
         subprocess.run([venv_python, "main.py"], check=True)
         print("main.py executed successfully!")
     except subprocess.CalledProcessError as e:
